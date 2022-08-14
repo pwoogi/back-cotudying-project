7357b3b0e8a92882d019df7a3893be538ea75bb6
kbjun2686 <kbjun2686@gmail.com>
2022-08-15 01:05:34 +0900

    전체 조회하기 데이터 추가

--- src/main/java/com/week06/cotudying_project/dto/boardsDto/BoardsShowResponseDto.java
+++ src/main/java/com/week06/cotudying_project/dto/boardsDto/BoardsShowResponseDto.java
@@ -14,15 +14,21 @@ public class BoardsShowResponseDto {
     private LocalDateTime modifiedAt;
     private Long id;
     private String title;
+    private Long num;
     private String category;
+    private String period;
     private String content;
+    private Long participant;
 
     public BoardsShowResponseDto(Boards boards) {
         this.createdAt = boards.getCreatedAt();
         this.modifiedAt = boards.getModifiedAt();
         this.id = boards.getId();
         this.title = boards.getTitle();
+        this.num = boards.getNum();
         this.category = boards.getCategory();
+        this.period = boards.getPeriod();
         this.content = boards.getContent();
+        this.participant = boards.getParticipant();
     }
 }
